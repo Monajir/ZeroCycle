ea8a329fad8887118e1ff7d98f59ff03a7f36dd9
Monazir <monajirwantswar@gmail.com>
2026-01-29 05:49:02 +0600

    Worked on the AuthController file

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartWaste.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet("/Auth")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Access";
+            return View();
+        }
+    }
+}
