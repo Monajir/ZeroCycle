c81249da4d6ba6833fec83e340c4aa8d73162e0d
Monazir <monajirwantswar@gmail.com>
2026-01-29 06:40:26 +0600

    Simplified and fixed the Public Controller code

--- Controllers/PublicController.cs
+++ Controllers/PublicController.cs
@@ -1,78 +1,93 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using SmartWaste.Web.Models;
+using System.Data;
 
 namespace SmartWaste.Web.Controllers
 {
     public class PublicController : Controller
     {
-        private readonly IConfiguration _config;
+        private readonly string _connectionString;
 
         public PublicController(IConfiguration config)
         {
-            _config = config;
+            _connectionString = config.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("DefaultConnection is missing.");
         }
 
         [HttpGet("/public/map")]
-        public IActionResult Map() => Redirect("/PublicMap");
+        public IActionResult Map()
+        {
+            return Redirect("/PublicMap");
+        }
 
-
-        // JSON endpoint for Leaflet map
         [HttpGet("/public/nearby")]
         [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
         public async Task<IActionResult> Nearby(
-            [FromQuery] double lat,
-            [FromQuery] double lng,
-            [FromQuery] int radiusMeters = 1000,
-            [FromQuery] byte? statusId = null)
+            double lat,
+            double lng,
+            int radiusMeters = 1000,
+            byte? statusId = null)
         {
-            if (radiusMeters < 50) radiusMeters = 50;
-            if (radiusMeters > 5000) radiusMeters = 5000;
+            radiusMeters = Math.Clamp(radiusMeters, 50, 5000);
 
-            // Get connection string
-            var cs = _config.GetConnectionString("DefaultConnection");
-            if (string.IsNullOrWhiteSpace(cs))
-                return Problem("DefaultConnection is missing.");
+            var bins = new List<NearbyBinDto>();
 
-            var results = new List<NearbyBinDto>();
+            await using var conn = new SqlConnection(_connectionString);
+            await using var cmd = new SqlCommand("dbo.sp_GetNearbyBins", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
 
-            // Open connection
-            await using var conn = new SqlConnection(cs);
+            cmd.Parameters.AddRange(new[]
+            {
+                new SqlParameter("@Latitude", SqlDbType.Float) { Value = lat },
+                new SqlParameter("@Longitude", SqlDbType.Float) { Value = lng },
+                new SqlParameter("@RadiusMeters", SqlDbType.Int) { Value = radiusMeters },
+                new SqlParameter("@StatusId", SqlDbType.TinyInt)
+                {
+                    Value = statusId ?? (object)DBNull.Value
+                }
+            });
+
             await conn.OpenAsync();
 
-            // Create command and set parameters
-            await using var cmd = new SqlCommand("dbo.sp_GetNearbyBins", conn);
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            
-            // Set parameters
-            cmd.Parameters.AddWithValue("@Latitude", lat);
-            cmd.Parameters.AddWithValue("@Longitude", lng);
-            cmd.Parameters.AddWithValue("@RadiusMeters", radiusMeters);
-            cmd.Parameters.AddWithValue("@StatusId", (object?)statusId ?? DBNull.Value);
-
-            // Execute the stored procedure
             await using var reader = await cmd.ExecuteReaderAsync();
+
+            // Cache ordinals once
+            int ordBinId = reader.GetOrdinal("BinId");
+            int ordLocation = reader.GetOrdinal("Location");
+            int ordLat = reader.GetOrdinal("Latitude");
+            int ordLng = reader.GetOrdinal("Longitude");
+            int ordCapacity = reader.GetOrdinal("CapacityLiters");
+            int ordWasteType = reader.GetOrdinal("WasteType");
+            int ordStatusId = reader.GetOrdinal("BinStatusId");
+            int ordStatusName = reader.GetOrdinal("StatusName");
+            int ordFill = reader.GetOrdinal("LatestFillLevelPercent");
+            int ordLastReading = reader.GetOrdinal("LastReadingTime");
+            int ordDistance = reader.GetOrdinal("DistanceMeters");
+
             while (await reader.ReadAsync())
             {
-                results.Add(new NearbyBinDto
+                bins.Add(new NearbyBinDto
                 {
-                    BinId = reader.GetInt32(reader.GetOrdinal("BinId")),
-                    Location = reader.GetString(reader.GetOrdinal("Location")),
-                    Latitude = reader.GetDecimal(reader.GetOrdinal("Latitude")),
-                    Longitude = reader.GetDecimal(reader.GetOrdinal("Longitude")),
-                    CapacityLiters = reader.GetInt32(reader.GetOrdinal("CapacityLiters")),
-                    WasteType = reader.GetString(reader.GetOrdinal("WasteType")),
-                    BinStatusId = reader.GetByte(reader.GetOrdinal("BinStatusId")),
-                    StatusName = reader.GetString(reader.GetOrdinal("StatusName")),
-                    LatestFillLevelPercent = reader.GetInt32(reader.GetOrdinal("LatestFillLevelPercent")),
-                    LastReadingTime = reader.IsDBNull(reader.GetOrdinal("LastReadingTime"))
+                    BinId = reader.GetInt32(ordBinId),
+                    Location = reader.GetString(ordLocation),
+                    Latitude = reader.GetDecimal(ordLat),
+                    Longitude = reader.GetDecimal(ordLng),
+                    CapacityLiters = reader.GetInt32(ordCapacity),
+                    WasteType = reader.GetString(ordWasteType),
+                    BinStatusId = reader.GetByte(ordStatusId),
+                    StatusName = reader.GetString(ordStatusName),
+                    LatestFillLevelPercent = reader.GetInt32(ordFill),
+                    LastReadingTime = reader.IsDBNull(ordLastReading)
                         ? null
-                        : reader.GetDateTime(reader.GetOrdinal("LastReadingTime")),
-                    DistanceMeters = reader.GetInt32(reader.GetOrdinal("DistanceMeters"))
+                        : reader.GetDateTime(ordLastReading),
+                    DistanceMeters = reader.GetInt32(ordDistance)
                 });
             }
-            
-            return Json(results);
+
+            return Ok(bins);
         }
     }
 }
